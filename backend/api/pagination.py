81c4d836c3c5bd5fba9320999cae31f3178fd4ac
Игорь <116277615+SpaiK89@users.noreply.github.com>
2023-06-14 12:14:41 +0300

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,6 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 
+from .params import PAGE_SIZE
+
 
 class PageLimitPagination(PageNumberPagination):
     """Стандартный пагинатор с выводом запрошенного количества страниц."""
     page_size_query_param = 'limit'
+    page_size = PAGE_SIZE
